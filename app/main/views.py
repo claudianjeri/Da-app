0b6cb05ee41d88ed9c6a954ab4a904bdec4992ef
canssidle wairimu <canssidlewairimu@gmail.com>
2018-05-28 14:08:31 +0300

    add all changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,24 @@
 from . import main
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
+
+
 
 @main.route('/')
 def index():
-    return '<h1> Hello World </h1>'
+    return render_template("index.html")
+
+
+
+@main.route("/login")
+def login():
+    return render_template("login.html")
+
+@main.route("/signup.html")
+def signup():
+    return render_template("signup.html")
+
+@main.route("/dashboard.html")
+def dashboard():
+    return render_template("dashboard.html")
 
