a9392953b176f345ae2473a8e520ae80fb1f3c8f
claudianjeri <claudianjeri04@gmail.com>
2018-05-28 18:17:21 +0300

    added profile.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from . import main
+from flask import render_template
 
 @main.route('/')
 def index():
-    return '<h1> Hello World </h1>'
+    title = 'Dapp'
+    return render_template('index.html', title = title)
 
